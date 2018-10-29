0936582c486bc678f37561fc46d6caded26ae386
David Sullivan <dsullivan0190@gmail.com>
2018-10-29 07:49:41 +0000

    Added requirements.txt and updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("./requirements.txt", session='hack')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='dp4py-logging',
@@ -8,5 +16,6 @@ setup(
     license='',
     author='ONSDigital',
     author_email='',
-    description=''
+    description='',
+    install_requires=reqs
 )
