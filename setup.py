62045c80a7e84f4dd8628235b31a27aa254b0403
David Sullivan <dsullivan0190@gmail.com>
2018-10-29 07:20:12 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dp4py-logging',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='ONSDigital',
+    author_email='',
+    description=''
+)
