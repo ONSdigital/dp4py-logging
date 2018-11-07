4f8e8e318495fd787fd79d8049facf7e7b05108e
David Sullivan <dsullivan0190@gmail.com>
2018-11-07 12:06:19 +0000

    Add decorator to time method execution

---
+++ dp4py_logging/time.py
@@ -0,0 +1,22 @@
+"""
+Adds decorator to log execution times
+"""
+import logging
+
+from inspect import isawaitable
+from time import time
+
+
+def timeit(method):
+    async def timed(*args, **kw):
+        ts = time()
+        result = method(*args, **kw)
+        if isawaitable(result):
+            result = await result
+        te = time()
+        logging.info("Recorded execution time", extra={
+            "method": method.__name__,
+            "time(ms)": (te - ts) * 1000
+        })
+        return result
+    return timed
