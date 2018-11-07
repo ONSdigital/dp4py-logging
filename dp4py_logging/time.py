e4848c52944bd5fe3d9d0f99169fbc67a455b2c8
David Sullivan <dsullivan0190@gmail.com>
2018-11-07 12:15:08 +0000

    Only log execution time on debug

--- dp4py_logging/time.py
+++ dp4py_logging/time.py
@@ -6,20 +6,28 @@ import logging
 from inspect import isawaitable
 from time import time
 
+_is_debug = logging.getLogger().getEffectiveLevel() == logging.DEBUG
+
 
 def timeit(method):
     async def timed(*args, **kw):
-        ts = time()
-        result = method(*args, **kw)
-        if isawaitable(result):
-            result = await result
-        te = time()
-        logging.info("Recorded execution time", extra={
-            "method": {
-                "name": method.__name__,
-                "module": method.__module__
-            },
-            "time(ms)": (te - ts) * 1000
-        })
-        return result
+        if _is_debug:
+            ts = time()
+            result = method(*args, **kw)
+            if isawaitable(result):
+                result = await result
+            te = time()
+            logging.debug("Recorded execution time", extra={
+                "method": {
+                    "name": method.__name__,
+                    "module": method.__module__
+                },
+                "time(ms)": (te - ts) * 1000
+            })
+            return result
+        else:
+            result = method(*args, **kw)
+            if isawaitable(result):
+                result = await result
+            return result
     return timed
